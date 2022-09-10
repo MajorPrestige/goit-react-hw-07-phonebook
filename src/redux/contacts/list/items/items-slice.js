afc19951cfdcb257d8f4a8d82ff1bb54a74283ff
Denis Dashkevich <majorprestige99@gmail.com>
2022-09-10 13:53:48 +0300

    Update items-slice.js

--- src/redux/contacts/list/items/items-slice.js
+++ src/redux/contacts/list/items/items-slice.js
@@ -7,16 +7,21 @@ import {
   deleteCheckedContacts,
 } from './list-operations';
 
-const itemsSlice = createSlice({
+const items = createSlice({
   name: 'items',
   initialState: [],
   extraReducers: {
     [getContacts.fulfilled]: (_, { payload }) => payload,
     [addContact.fulfilled]: (store, { payload }) => store.unshift(payload),
-    [deleteContact.fulfilled]: (store, { payload }) => store.filter(({ id }) => id !== payload),
+    [deleteContact.fulfilled]: (store, { payload }) =>
+      store.filter(({ id }) => id !== payload),
     [checkContacts.fulfilled]: (store, { payload }) => {
       const checkedContact = store.find(({ id }) => id === payload);
       checkedContact.checked = !checkedContact.checked;
     },
-    [deleteCheckedContacts.fulfilled]: (store, { payload }) => store.filter(el => !payload.includes(el.id)),
-})
+    [deleteCheckedContacts.fulfilled]: (store, { payload }) =>
+      store.filter(el => !payload.includes(el.id)),
+  },
+});
+
+export default items.reducer;
